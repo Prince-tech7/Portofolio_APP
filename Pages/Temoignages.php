ed11946dda7d0778f027bec86bbeae69d71ee58a
PrinceGuy <guyprince24@gmail.com>
2024-11-19 21:17:32 +0100

    Mon cinquième commit suppression de la page Pages/Temoignages.php

--- Pages/Temoignages.php
+++
@@ -1,8 +0,0 @@
-<?php
-$titre ="Clients Satisfaits";
-require_once 'C:\xampp\htdocs\PortofolioApp\Components\header.php';
-
-// @definition du fonctionnerment "active" lors du pointage du curceur
-$nav = "Temoignages";
-
-?>
